90e4be7248c15946a2d5aad7cee283c7b06344b1
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2017-06-06 01:07:25 -0300

    Varios refactor a como se pasaban los datos en los test

--- eventos/spec/calendario_spec.rb
+++ eventos/spec/calendario_spec.rb
@@ -3,22 +3,33 @@ require_relative '../model/calendario'
 
 describe 'Calendario' do
   it 'Crear un calendario a partir de un nombre' do
-    Calendario.new(nombre: 'Calendario 1')
+    datos_calendario = {
+      nombre: 'Calendario 1'
+    }
+    Calendario.new(datos_calendario)
   end
 
   it 'Error al crear un calendario sin parametros' do
-    expect { Calendario.new(nil) }.to raise_error(TypeError)
+    datos_calendario = nil
+    expect { Calendario.new(datos_calendario) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un calendario con un formato invalido de parametros' do
-    expect { Calendario.new("Calendario 1") }.to raise_error(TypeError)
+    datos_calendario = 'Calendario 1'
+    expect { Calendario.new(datos_calendario) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un calendario sin nombre' do
-    expect { Calendario.new(otro_parametro: 'Calendario 1') }.to raise_error(TypeError)
+    datos_calendario = {
+      otro_parametro: 'Calendario 1'
+    }
+    expect { Calendario.new(datos_calendario) }.to raise_error(TypeError)
   end
 
-  it 'Error al crear un calendario con un formato con un formato invalido de nombre' do
-    expect { Calendario.new(nombre: 1) }.to raise_error(TypeError)
+  it 'Error al crear un calendario con un formato invalido de nombre' do
+    datos_calendario = {
+      nombre: 1
+    }
+    expect { Calendario.new(datos_calendario) }.to raise_error(TypeError)
   end
 end
--- eventos/spec/evento_spec.rb
+++ eventos/spec/evento_spec.rb
@@ -3,95 +3,88 @@ require_relative '../model/evento'
 
 describe 'Evento' do
   it 'Crear un evento a partir de un id, nombre, inicio y fin' do
-    Evento.new(
+    datos_evento = {
       id: 'id_1',
       nombre: 'Evento_1',
       inicio: DateTime.now,
       fin: DateTime.now
-    )
+    }
+    Evento.new(datos_evento)
   end
 
   it 'Error al crear un evento sin id' do
-    expect do
-      Evento.new(
-        nombre: 'Evento_1',
-        inicio: DateTime.now,
-        fin: DateTime.now
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      nombre: 'Evento_1',
+      inicio: DateTime.now,
+      fin: DateTime.now
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un evento con formato invalido de id' do
-    expect do
-      Evento.new(
-        id: 1,
-        nombre: 'Evento_1',
-        inicio: DateTime.now,
-        fin: DateTime.now
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      id: 1,
+      nombre: 'Evento_1',
+      inicio: DateTime.now,
+      fin: DateTime.now
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un evento sin nombre' do
-    expect do
-      Evento.new(
-        id: 'id_1',
-        inicio: DateTime.now,
-        fin: DateTime.now
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      id: 'id_1',
+      inicio: DateTime.now,
+      fin: DateTime.now
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un evento con formato invalido de nombre' do
-    expect do
-      Evento.new(
-        id: 'id_1',
-        nombre: 1,
-        inicio: DateTime.now,
-        fin: DateTime.now
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      id: 'id_1',
+      nombre: 1,
+      inicio: DateTime.now,
+      fin: DateTime.now
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un evento sin inicio' do
-    expect do
-      Evento.new(
-        id: 'id_1',
-        nombre: 'Evento_1',
-        fin: DateTime.now
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      id: 'id_1',
+      nombre: 'Evento_1',
+      fin: DateTime.now
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un evento con formato invalido de inicio' do
-    expect do
-      Evento.new(
-        id: 'id_1',
-        nombre: 'Evento_1',
-        inicio: DateTime.now.to_s,
-        fin: DateTime.now
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      id: 'id_1',
+      nombre: 'Evento_1',
+      inicio: DateTime.now.to_s,
+      fin: DateTime.now
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un evento sin fin' do
-    expect do
-      Evento.new(
-        id: 'id_1',
-        nombre: 'Evento_1',
-        inicio: DateTime.now,
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      id: 'id_1',
+      nombre: 'Evento_1',
+      inicio: DateTime.now
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 
   it 'Error al crear un evento con formato invalido de fin' do
-    expect do
-      Evento.new(
-        id: 'id_1',
-        nombre: 'Evento_1',
-        inicio: DateTime.now,
-        fin: DateTime.now.to_s
-      )
-    end.to raise_error(TypeError)
+    datos_evento = {
+      id: 'id_1',
+      nombre: 'Evento_1',
+      inicio: DateTime.now,
+      fin: DateTime.now.to_s
+    }
+    expect { Evento.new(datos_evento) }.to raise_error(TypeError)
   end
 end
