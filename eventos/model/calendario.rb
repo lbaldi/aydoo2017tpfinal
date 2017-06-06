b377ec4809cc5428731b5dea32a627ea8de39db7
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2017-06-06 00:49:10 -0300

    Se valida que el parametro de creacion para un calendario sea un hash

--- eventos/model/calendario.rb
+++ eventos/model/calendario.rb
@@ -1,6 +1,7 @@
 # Repositorio de eventos.
 class Calendario
   def initialize(params)
+    raise TypeError unless params.is_a? Hash
     raise TypeError unless params[:nombre].is_a? String
     @nombre = params[:nombre]
   end
