fce668bd2ae73f21a1a4bf83d25b59c2c0143f3a
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2017-06-06 00:51:06 -0300

    Test faltantes para creacion de calendarios

--- eventos/spec/calendario_spec.rb
+++ eventos/spec/calendario_spec.rb
@@ -6,6 +6,14 @@ describe 'Calendario' do
     Calendario.new(nombre: 'Calendario 1')
   end
 
+  it 'Error al crear un calendario sin parametros' do
+    expect { Calendario.new(nil) }.to raise_error(TypeError)
+  end
+
+  it 'Error al crear un calendario con un formato invalido de parametros' do
+    expect { Calendario.new("Calendario 1") }.to raise_error(TypeError)
+  end
+
   it 'Error al crear un calendario sin nombre' do
     expect { Calendario.new(otro_parametro: 'Calendario 1') }.to raise_error(TypeError)
   end
