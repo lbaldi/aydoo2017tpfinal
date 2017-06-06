afc362eea1519bfd19ac9cf0f6c70a5e83dbf031
dehernan <moreno.hernan12@gmail.com>
2017-06-06 12:16:11 -0300

    Se agrega un listado de eventos al calendario

--- eventos/model/calendario.rb
+++ eventos/model/calendario.rb
@@ -1,15 +1,24 @@
 # Repositorio de eventos.
 class Calendario
   attr_reader :nombre
-  # @TODO Agregar listado de eventos y funciones para su uso
+  #Repositorio de eventos para el calendario
+  attr_accessor :eventos
+  # @TODO agregar funciones para su uso
 
   def initialize(params)
     validar_parametros(params)
     @nombre = params[:nombre]
+    @eventos = Array.new
   end
 
   def validar_parametros(params)
     raise TypeError unless params.is_a? Hash
     raise TypeError unless params[:nombre].is_a? String
   end
+
+  def insertar_evento(params)
+  	raise TypeError unless params.is_a? Evento
+  	@eventos << params
+  end
+
 end
--- eventos/spec/calendario_spec.rb
+++ eventos/spec/calendario_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative '../model/calendario'
+require_relative '../model/evento'
 
 describe 'Calendario' do
   it 'Crear un calendario a partir de un nombre' do
@@ -48,4 +49,47 @@ describe 'Calendario' do
     calendario = Calendario.new(datos_calendario)
     expect { calendario.nombre = 'Nuevo Nombre' }.to raise_error
   end
+
+  it 'Se agrega un evento al calendario' do
+    datos_calendario = {
+      nombre: 'Calendario 1'
+    }
+    datos_evento = {
+      id: 'id_1',
+      nombre: 'Evento_1',
+      inicio: DateTime.now,
+      fin: DateTime.now,
+      recurrencia: nil
+    }
+    evento_calendario = Evento.new(datos_evento)
+    calendario = Calendario.new(datos_calendario)
+    calendario.insertar_evento(evento_calendario)
+  end
+
+  it 'El calendario debería almacenar el evento guardado' do
+    datos_calendario = {
+      nombre: 'Calendario 1'
+    }
+    datos_evento = {
+      id: 'id_1',
+      nombre: 'Evento_1',
+      inicio: DateTime.now,
+      fin: DateTime.now,
+      recurrencia: nil
+    }
+    evento_calendario = Evento.new(datos_evento)
+    calendario = Calendario.new(datos_calendario)
+    calendario.insertar_evento(evento_calendario)
+
+    expect(calendario.eventos[0]).to eq evento_calendario
+  end
+
+  it 'Error al querer insertar un evento que no es del tipo Evento' do
+    datos_calendario = {
+      nombre: 'Calendario 1'
+    }
+    calendario = Calendario.new(datos_calendario)
+    expect { calendario.insertar_evento(3)}.to raise_error(TypeError)
+  end
+
 end
