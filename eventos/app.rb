d624c0a426b19c0e6c1030b91ca94d574a7aba7e
dehernan <moreno.hernan12@gmail.com>
2017-06-06 22:08:34 -0300

    Se crea la estructura de sinatra

--- eventos/app.rb
+++ eventos/app.rb
@@ -1,1 +1,81 @@
 require 'sinatra'
+require 'json'
+
+post '/calendarios' do
+  begin
+    request.body.rewind
+    data = JSON.parse request.body.read
+    # Implementacion
+    status 200
+  rescue Exception
+    status 400
+  end
+end
+
+delete '/calendarios' do
+  begin
+    nombre = params[:nombre]
+    # Implementacion
+    status 200
+  rescue Exception
+    # No encontrado
+    status 404
+  end
+end
+
+get '/calendarios' do
+  # Implementacion
+  status 200
+end
+
+get '/calendarios' do
+  begin
+    nombre = params[:nombre]
+    # Implementacion
+    status 200
+  rescue Exception
+    # No encontrado
+    status 404
+  end
+end
+
+post '/eventos' do
+  begin
+    request.body.rewind
+    data = JSON.parse request.body.read
+    # Implementacion
+    status 201
+  rescue Exception
+    status 400
+  end
+end
+
+put '/eventos' do
+  request.body.rewind
+  data = JSON.parse request.body.read
+  # Implementacion
+  status 200
+end
+
+delete '/eventos/id' do
+  id_evento = params[:id]
+  # Implementacion
+  status 200
+end
+
+get '/eventos' do
+  # Implementacion
+  status 200
+end
+
+get '/eventos' do
+  nombre_calendario = params[:nombre]
+  # Implementacion
+  status 200
+end
+
+get '/eventos/id' do
+  id_evento = params[:id]
+  # Implementacion
+  status 200
+end
